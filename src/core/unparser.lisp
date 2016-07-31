dec7d9275ed04e059d59c8dec151f3010a66416f
moratori <hoko1993@yahoo.co.jp>
2016-08-01 00:12:11 +0900

    add: nameを圧縮しない場合のunparse処理のひな形を実装

--- src/core/unparser.lisp
+++ src/core/unparser.lisp
@@ -5,21 +5,108 @@
         :dnslib.core.errors
         )
   (:export 
-    :unparse
+    :unparse-direct
+    :sizeof-direct
+
+    :unparse-compress
+    :sizeof-compress
     )
   )
 (in-package :dnslib.core.unparser)
 
 
+#|
+  nameの圧縮部分の実装でいい方法を思いつかないので
+  圧縮なしの配列を返す。
+  所定のサイズ(512を意図)を超えてしまう場合には
+  TCフラグを立てるなどの対応が必要
+|#
 
 
 
+(defun sizeof-direct (dns)
+  "DNS構造体をnameの圧縮なしに配列に変換した場合
+   の配列のサイズを返す"
+
+  (declare (type dns dns))
+  
+  
+  )
+
+(defmethod unparse ((rr rr) arr start)
+  "arrのstart以降にリソースレコードを入れる"
+
+  
+  )
+
+(defmethod unparse ((question question) arr start)
+  "arrのstart以降にquestionを入れる"
+
+  
+  )
+
+(defmethod unparse ((header header) arr start)
+  "arrのstart以降にheaderを入れる"
+
+  )
 
 
-(defun unparse (dns)
-  "DNS構造体からunsigned-byte 8 な配列を返す"
+
+(defun %iterate-multiple-sections (sections arr start)
+  "funcはunparse-questionかunparse-aaaのいづれか
+   arrのstartから始まるところにsections(リスト)の要素を
+   連続で変換して入れるためのラッパ"
+  
+  (let ((next start))
+    (loop 
+      for each in sections
+      do (setf next (unparse each arr next)))
+    next))
+
+
+(defun unparse-direct (dns size)
+  "DNS構造体からunsigned-byte 8 な配列を返す
+   nameの圧縮は行わない
+   ;; sizeは、size-of-directで計算された配列のサイズ"
 
   (declare (type dns dns))
 
+  (when (< size 12)
+      (dp-error dns "malformed dns header(too short)"))
+  
+  (let ((result 
+          (make-array 
+            (list size)
+            :element-type '(unsigned-byte 8)
+            :initial-element 0))
+        (start 0))
+    
+    (setf start (unparse (dns.header dns) result start)
+          start (%iterate-multiple-sections (dns.question dns) result start)
+          start (%iterate-multiple-sections (dns.answer dns) result start)
+          start (%iterate-multiple-sections (dns.authority dns) result start)
+          start (%iterate-multiple-sections (dns.additional dns) result start))
 
+    result))
+
+
+
+
+
+
+
+
+(defun sizeof-compress (dns)
+  "DNS構造体をnameの圧縮ありで配列に変換した場合
+   の配列のサイズを返す"
+
+  (declare (ignore dns))
+  -1
+  )
+
+
+(defun unparse-compress (dns)
+  "DNS構造体からunsigned-byte 8 な配列を返す
+   nameの圧縮は行わない"
+  
   )
